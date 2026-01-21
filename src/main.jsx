89baab929894c2fafaac968ca6e48c94f46ea277
JK-VISWAS <joyviswaskolli@gmail.com>
2026-01-21 22:19:13 +0530

    main edit

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,16 @@
 import 'leaflet/dist/leaflet.css';
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './App.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { BrowserRouter, Routes } from 'react-router-dom';
-import { Route } from 'lucide-react';
-import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/URBANPULSE/">
+    <BrowserRouter basename="/URBANPULSE">
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/*" element={<App />} />
       </Routes>
-      </BrowserRouter>
-  </React.StrictMode>,
+    </BrowserRouter>
+  </React.StrictMode>
 )
