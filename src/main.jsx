0adf78f1ac1cbe1a8d27b5783a8f44beafe08446
JK-VISWAS <joyviswaskolli@gmail.com>
2026-01-21 22:35:12 +0530

    changed

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/URBANPULSE">
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/*" element={<App />} />
       </Routes>
